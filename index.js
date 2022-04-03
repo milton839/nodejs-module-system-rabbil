1c77395aec3d0d9d91780ac2f389736295935138
Milton839 <miltonbiswas839@gmail.com>
2022-04-04 00:56:09 +0600

    fs system done

--- index.js
+++ index.js
@@ -1,40 +1,96 @@
 const http = require('http');
-const URl = require('url');
+const fs = require('fs');
 
 const server = http.createServer(function(req, res){
     /**
-     * * ===========http module routing system==============
+     * * ===========fs module===========
      */
-    // if (req.url === '/') {
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write('<h1>This is home page</h1>')
-    //     res.end();
-    // }
-    // else if(req.url === '/about'){
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write('<h1>This is about page</h1>')
-    //     res.end();
-    // }
-    // else if(req.url === '/contact'){
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write('<h1>This is contact page</h1>')
-    //     res.end();
-    // }
-    // else{
-    //     res.writeHead(404, {'Content-Type': 'text/html'});
-    //     res.write('<h1>404 page</h1>')
-    //     res.end();
-    // }
+        if (req.url=='/') {
+            /**
+             * ? ===========fs module readfile system 13-26 number line===========
+             */
+            /**
+             * todo ===========fs module async system===========
+             */
+            // fs.readFile('home.html', function(err, data){
+            //     res.writeHead(200, {'Content-Type': 'text/html'})
+            //     res.write(data);
+            //     res.end();
+            // })
+            /**
+             * ! ===========fs module async system===========
+             */
+                // const t = fs.readFileSync('home.html');
+                // res.writeHead(200, {'Content-Type': 'text/html'})
+                // res.write(t);
+                // res.end();
+
+            /**
+            * ? ===========fs module writefile system 13-26 number line===========
+             */
+            // fs.writeFile('demo.txt', "welcome to our status code", function (err){
+            //     if (err) {
+            //         res.writeHead(400, {'Content-Type': 'text/html'});
+            //         res.write("failed 400");
+            //         res.end();
+            //     }
+            //     else{
+            //         res.writeHead(200, {'Content-Type': 'text/html'});
+            //         res.write("Success 200");
+            //         res.end();
+            //     }
+            // })
+
+            /**
+            * ? ===========fs module writefile system 13-26 number line===========
+             */
+            // const error = fs.writeFileSync('test', "hello world write file by synchronous system");
+            // if (error) {
+            //     res.writeHead(400, {'Content-Type': 'text/html'});
+            //     res.write("File write failed 400");
+            //     res.end();
+            // }
+            // else{
+            //     res.writeHead(200, {'Content-Type': 'text/html'});
+            //     res.write("File write Success 200......");
+            //     res.end();
+            // }
+
+            /**
+            * ? ===========fs module rename file system 13-26 number line===========
+             */
+            // fs.rename('demo.txt', 'file.txt', function (err){
+            //     if (err) {
+            //             res.writeHead(400, {'Content-Type': 'text/html'});
+            //             res.write("File rename failed 400......");
+            //             res.end();
+            //     }
+            //     else{
+            //         res.writeHead(200, {'Content-Type': 'text/html'});
+            //         res.write("File rename Success 200......");
+            //         res.end();
+            //     }
+            // });
+
+            /**
+            * ? ===========fs module delete file system 13-26 number line===========
+             */
+
+            fs.unlink('test', function(err) {
+                if (err) {
+                        res.writeHead(400, {'Content-Type': 'text/html'});
+                        res.write("File delete failed 400......");
+                        res.end();
+                }
+                else{
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write("File delete Success 200......");
+                    res.end();
+                }
+            })
+        }
     /**
-     * ! ===========http module routing system end==============
-     */
-    
-    /**
-     * ? ==============URl module system start========================
-     */
-    
-    /**
-     * todo: ==============URl module system========================
+     * ! ===========fs module end===========
      */
     
 
